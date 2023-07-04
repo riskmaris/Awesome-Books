1d132f198de3555eb96db737e58d54313d3f74ab
anshuman7negi <anshumannegi777@gmail.com>
2023-07-04 15:10:38 +0530

    Added styling to css file

--- main.js
+++ main.js
@@ -2,43 +2,43 @@
 
 class BookLibrary {
   constructor() {
-     this.bookDetails=[];
+    this.bookDetails = [];
 
   }
 
-  displayBook(){
-    console.log("hellow")
+  displayBook() {
     const reciveBooks = localStorage.getItem('booksData');
-      if (reciveBooks) {
-        this.bookDetails = JSON.parse(reciveBooks);
-        const bookStore = document.getElementById('allBooks');
-        bookStore.innerHTML = '';
-        for (let i = 0; i < this.bookDetails.length; i += 1) {
-          bookStore.innerHTML += `<div id="book${i}" class="book-store">
-                                         <p>${this.bookDetails[i].title}</p>
-                                         <p>${this.bookDetails[i].author}</p>
-                                         <button  class="remove-button">remove</button>
-                                         <hr>
-                                         </div>`;
-        }
+    if (reciveBooks) {
+      this.bookDetails = JSON.parse(reciveBooks);
+      const bookStore = document.getElementById('allBooks');
+      bookStore.innerHTML = '';
+      for (let i = 0; i < this.bookDetails.length; i += 1) {
+        bookStore.innerHTML += 
+        `<tbody id="book${i}" class="book-store">
+          <tr> 
+          <td> "${this.bookDetails[i].title}"  <span>by</span> ${this.bookDetails[i].author} </td>
+          <td> <button  class="remove-button">Remove</button> </td>
+          </tr>
+        </tbody>`;
       }
+    }
   }
-  addBook(title,author){
-    this.bookDetails.push({title,author});
-    localStorage.setItem('booksData',JSON.stringify(this.bookDetails));
+  addBook(title, author) {
+    this.bookDetails.push({ title, author });
+    localStorage.setItem('booksData', JSON.stringify(this.bookDetails));
     this.displayBook();
   }
 
-  removeBook(index){
+  removeBook(index) {
     this.bookDetails.splice(index, 1);
-    localStorage.setItem('booksData',JSON.stringify(this.bookDetails));
+    localStorage.setItem('booksData', JSON.stringify(this.bookDetails));
     this.displayBook();
   }
 
 
 }
 
-const book=new BookLibrary();
+const book = new BookLibrary();
 
 book.displayBook();
 
@@ -46,7 +46,7 @@ document.getElementById('addBook').addEventListener('click', () => {
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
   if (title !== '' && author !== '') {
-    book.addBook(title,author);
+    book.addBook(title, author);
     document.getElementById('title').value = '';
     document.getElementById('author').value = '';
   }
@@ -55,7 +55,6 @@ document.getElementById('addBook').addEventListener('click', () => {
 document.getElementById('allBooks').addEventListener('click', (event) => {
   if (event.target.classList.contains('remove-button')) {
     const bookDiv = event.target.parentNode;
-    console.log(bookDiv.parentNode.children)
     const index = Array.from(bookDiv.parentNode.children).indexOf(bookDiv);
     book.removeBook(index);
   }

183fc2bf0fc5cdc54f0af0786435471e93f4e243
anshuman7negi <anshumannegi777@gmail.com>
2023-07-04 14:16:36 +0530

    created class and method to add function

--- main.js
+++ main.js
@@ -1,48 +1,36 @@
-let bookDetails = [];
 
-function reciveData() {
-  const reciveBooks = localStorage.getItem('booksData');
-  if (reciveBooks) {
-    bookDetails = JSON.parse(reciveBooks);
-    const bookStore = document.getElementById('allBooks');
-    bookStore.innerHTML = '';
-    for (let i = 0; i < bookDetails.length; i += 1) {
-      bookStore.innerHTML += `<div id="book${i}" class="book-store">
-                                     <p>${bookDetails[i].book}</p>
-                                     <p>${bookDetails[i].author}</p>
-                                     <button  class="remove-button">remove</button>
-                                     <hr>
-                                     </div>`;
-    }
+
+class BookLibrary {
+  constructor() {
+     this.bookDetails=[];
+
   }
-}
-function storeData() {
-  localStorage.setItem('booksData', JSON.stringify(bookDetails));
-  reciveData();
+
+
+  addBook(title,author){
+    this.bookDetails.push({title,author});
+    localStorage.setItem('booksData',JSON.stringify(this.bookDetails));
+    this.displayBook();
+  }
+
+
+
+
 }
 
-function removeBook(index) {
-  bookDetails.splice(index, 1);
-  storeData();
-}
+const book=new BookLibrary();
+
+book.displayBook();
 
 document.getElementById('addBook').addEventListener('click', () => {
-  const book = document.getElementById('title').value;
+  const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
-  if (book !== '' && author !== '') {
-    bookDetails.push({ book, author });
-    storeData();
+  if (title !== '' && author !== '') {
+    book.addBook(title,author);
     document.getElementById('title').value = '';
     document.getElementById('author').value = '';
   }
 });
 
-document.getElementById('allBooks').addEventListener('click', (event) => {
-  if (event.target.classList.contains('remove-button')) {
-    const bookDiv = event.target.parentNode;
-    const index = Array.from(bookDiv.parentNode.children).indexOf(bookDiv);
-    removeBook(index);
-  }
-});
 
-reciveData();
+

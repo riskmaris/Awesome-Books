7400e11c8e4a68987b14f070ed997fe718999c42
riskmaris <mariskaps20@gmail.com>
2023-07-04 11:54:43 +0300

    Create function to display books

--- main.js
+++ main.js
@@ -6,7 +6,23 @@ class BookLibrary {
 
   }
 
-
+  displayBook(){
+    console.log("hellow")
+    const reciveBooks = localStorage.getItem('booksData');
+      if (reciveBooks) {
+        this.bookDetails = JSON.parse(reciveBooks);
+        const bookStore = document.getElementById('allBooks');
+        bookStore.innerHTML = '';
+        for (let i = 0; i < this.bookDetails.length; i += 1) {
+          bookStore.innerHTML += `<div id="book${i}" class="book-store">
+                                         <p>${this.bookDetails[i].title}</p>
+                                         <p>${this.bookDetails[i].author}</p>
+                                         <button  class="remove-button">remove</button>
+                                         <hr>
+                                         </div>`;
+        }
+      }
+  }
   addBook(title,author){
     this.bookDetails.push({title,author});
     localStorage.setItem('booksData',JSON.stringify(this.bookDetails));

622df25f6657b5807e31aebbc20c9c312dccf49b
riskmaris <mariskaps20@gmail.com>
2023-07-06 09:32:24 +0300

    Add navbar and time

--- main.js
+++ main.js
@@ -1,68 +1,92 @@
-class BookLibrary {
+class AllBooks {
   constructor() {
     const previousData = localStorage.getItem('booksData');
     this.bookDetails = previousData ? JSON.parse(previousData) : [];
   }
 
-  displayBook() {
+  displayBooks() {
     const reciveBooks = localStorage.getItem('booksData');
-    const allBooksTable = document.getElementById('allBooks');
-    const tableTitle = document.getElementById('table-title');
-
     if (reciveBooks && JSON.parse(reciveBooks).length > 0) {
       this.bookDetails = JSON.parse(reciveBooks);
-      const bookStore = document.getElementById('bookStore');
+      const bookStore = document.getElementById('allBooks');
       bookStore.innerHTML = '';
       for (let i = 0; i < this.bookDetails.length; i += 1) {
-        const bookRow = document.createElement('tr');
-        bookRow.id = `book${i}`;
-        bookRow.className = 'book-store';
-        bookRow.innerHTML = `
-          <td>"${this.bookDetails[i].title}" <span>by</span> ${this.bookDetails[i].author}</td>
-          <td><button class="remove-button">Remove</button></td>
-        `;
-        bookStore.appendChild(bookRow);
+        bookStore.innerHTML += ` <tr id="book${i}" class="book-store"> 
+                                       <td> ${this.bookDetails[i].title} by ${this.bookDetails[i].author} </td>
+                                       <td> <button  class="remove-button">Remove</button> </td>
+                                  </tr>`;
       }
-      allBooksTable.style.display = 'flex';
-      tableTitle.style.display = 'block';
+      const deleteRow = document.querySelectorAll('.book-store');
+      const removeBtn = document.querySelectorAll('.remove-button');
+
+      for (let i = 0; i < removeBtn.length; i += 1) {
+        removeBtn[i].addEventListener('click', () => {
+          deleteRow[i].remove();
+          this.removeBook(i);
+          this.displayBooks();
+        });
+      }
+
+      document.getElementById('book-title').style.display = 'block';
+      document.getElementById('tableBook').style.display = 'flex';
     } else {
-      allBooksTable.style.display = 'none';
-      tableTitle.style.display = 'none';
+      document.getElementById('book-title').style.display = '';
+      document.getElementById('tableBook').style.display = '';
     }
   }
 
   addBook(title, author) {
-    this.bookDetails.push({ title, author });
+    const book = { title, author };
+    this.bookDetails.push(book);
     localStorage.setItem('booksData', JSON.stringify(this.bookDetails));
-    this.displayBook();
   }
 
   removeBook(index) {
     this.bookDetails.splice(index, 1);
     localStorage.setItem('booksData', JSON.stringify(this.bookDetails));
-    this.displayBook();
+    this.displayBooks();
   }
 }
 
-const book = new BookLibrary();
+const mybooks = new AllBooks();
 
-book.displayBook();
-
-document.getElementById('addBook').addEventListener('click', (event) => {
-  event.preventDefault(); // Prevent form submission
+document.getElementById('addBook').addEventListener('click', () => {
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
+
   if (title !== '' && author !== '') {
-    book.addBook(title, author);
+    mybooks.addBook(title, author);
     document.getElementById('title').value = '';
     document.getElementById('author').value = '';
   }
 });
 
-document.getElementById('bookStore').addEventListener('click', (event) => {
-  if (event.target.classList.contains('remove-button')) {
-    const bookRow = event.target.parentNode.parentNode;
-    const index = Array.from(bookRow.parentNode.children).indexOf(bookRow);
-    book.removeBook(index);
-  }
+document.getElementById('list').addEventListener('click', () => {
+  mybooks.displayBooks();
+  document.getElementById('book-title').style.display = 'block';
+  document.getElementById('book-form').style.display = 'none';
+  document.getElementById('contact').style.display = 'none';
 });
+
+document.getElementById('newBook').addEventListener('click', () => {
+  document.getElementById('book-form').style.display = '';
+  document.getElementById('tableBook').style.display = 'none';
+  document.getElementById('book-title').style.display = 'none';
+  document.getElementById('contact').style.display = 'none';
+});
+
+document.getElementById('contact-link').addEventListener('click', () => {
+  document.getElementById('contact').style.display = 'flex';
+  document.getElementById('book-form').style.display = 'none';
+  document.getElementById('tableBook').style.display = 'none';
+  document.getElementById('book-title').style.display = 'none';
+});
+
+function updateTime() {
+  const currentDate = new Date();
+  const dateTimeString = currentDate.toLocaleString();
+  document.getElementById('time').textContent = dateTimeString;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
